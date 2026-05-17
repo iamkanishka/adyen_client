01ef58e73496e0a96483b3132d9fa2a3ab8f715a
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-05-17 13:27:57 +0530

    Dialyzer ignore

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,5 @@
+# Dialyzer ignore patterns for AdyenClient
+# Format: [{:warn_type, ~r/regex_to_match_warning_message/}]
+#
+# Runtime-only deps (not available at PLT build time) produce false positives:
+[]
